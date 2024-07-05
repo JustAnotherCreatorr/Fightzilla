ed7ea5c3b16e6ee9f136110bdf7e211d08369feb
CodeNinja <miltononca@codeninjas.com>
2024-07-05 19:01:33 -0400

    began to work on movement prototype - now beginning to move backwards?

---
+++ Assets/Scripts/prototypingmovement.cs
@@ -0,0 +1,24 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prototypingmovement : MonoBehaviour
+{
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("MoveSpeed", horizontal);
+    }
+}
