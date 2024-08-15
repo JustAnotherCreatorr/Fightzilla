2d5de910f2e5ef300922b1332a8762afcb4f6239
CodeNinja <miltononca@codeninjas.com>
2024-08-15 18:32:14 -0400

    made splash screen

--- Assets/Scripts/PROTOTYPEBB.cs
+++ Assets/Scripts/PROTOTYPEBB.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class prototypingbattlebucks : MonoBehaviour, IPointerDownHandler, IPointerUpHandler 
+public class PROTOTYPEBB : MonoBehaviour, IPointerDownHandler, IPointerUpHandler 
 {
 
     //public float currentAmount;
