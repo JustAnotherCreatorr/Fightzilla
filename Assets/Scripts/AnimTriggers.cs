80e0714e417a629acb68d5405445facadbb59b3f
Codeninjasmilton <miltononca@codeninjas.com>
2024-09-17 18:57:15 -0400

    going to work on fighting interactions now using animation events and collision logic

---
+++ Assets/Scripts/AnimTriggers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTriggers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckHitOpponent(string attackName)
+    {
+
+    }
+}
