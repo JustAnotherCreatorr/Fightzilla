7e093adaf30c46717e23ff9c84337e49d18b43e8
JustAnotherCreatorr <khan17saim@gmail.com>
2026-07-10 20:14:14 -0400

    made name change when mouse hover panel
    next is to code selecting and confirming characters

---
+++ Assets/Scripts/UI/CSUI/HoverPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class HoverPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public TMP_Text display;
+    private int UInumber;
+    public string characterName;
+    public Color characterColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UInumber = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        string colorCode = ColorUtility.ToHtmlStringRGB(characterColor);
+
+        display.text = $"Player {UInumber}: <color=#{colorCode}>{characterName}</color>";
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        display.text = $"Player {UInumber}:";
+    }
+}
