1ed9868e4efbc3af139de7858467004d38b59bfb
JustAnotherCreatorr <khan17saim@gmail.com>
2026-07-15 19:55:51 -0400

    made confirmation for berserker
    need to work on sharing confirmedp1 variable across all buttons

--- Assets/Scripts/UI/CSUI/HoverPanel.cs
+++ Assets/Scripts/UI/CSUI/HoverPanel.cs
@@ -7,12 +7,19 @@ using TMPro;
 
 public class HoverPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public TMP_Text display;
     private int UInumber;
+
+    public bool confirmedP1;
+
+    public TMP_Text currentDisplay;
+    public TMP_Text display;
+    public TMP_Text otherDisplay;
     public string characterName;
+
     public Color characterColor;
     public Color hoverPanelColor;
     public Color defaultPanelColor;
+
     public GameObject hoverOutline;
     public Outline panelOutline;
 
@@ -20,6 +27,8 @@ public class HoverPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     void Start()
     {
         UInumber = 1;
+        currentDisplay = display;
+        confirmedP1 = false;
     }
 
     // Update is called once per frame
@@ -31,15 +40,47 @@ public class HoverPanel : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     public void OnPointerEnter(PointerEventData eventData)
     {
         string colorCode = ColorUtility.ToHtmlStringRGB(characterColor);
-        display.text = $"Player {UInumber}: <color=#{colorCode}>{characterName}</color>";
         panelOutline.effectColor = hoverPanelColor;
         hoverOutline.SetActive(true);
+
+        if (currentDisplay == display)
+        {
+            display.text = $"Player {UInumber}: <color=#{colorCode}>{characterName}</color>";
+        }
+
+        if (currentDisplay == otherDisplay)
+        {
+            otherDisplay.text = $"Player {UInumber}: <color=#{colorCode}>{characterName}</color>";
+        }
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        display.text = $"Player {UInumber}:";
         panelOutline.effectColor = defaultPanelColor;
         hoverOutline.SetActive(false);
+
+        if (currentDisplay == display && !confirmedP1)
+        {
+            display.text = $"Player {UInumber}:";
+        }
+
+        if (currentDisplay == otherDisplay && confirmedP1)
+        {
+            otherDisplay.text = $"Player {UInumber}:";
+        }
+
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log("OMD");
+        if (currentDisplay == display)
+        {
+            Debug.Log("passedIF");
+            confirmedP1 = true;
+            currentDisplay = otherDisplay;
+            UInumber = 2;
+        }
     }
 }
