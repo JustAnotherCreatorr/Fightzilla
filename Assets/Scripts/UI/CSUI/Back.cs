efa38e52a6901c4296e7d666cd2b5b1d69fe5d7c
JustAnotherCreatorr <khan17saim@gmail.com>
2026-07-18 15:24:24 -0400

    Update Back.cs

--- Assets/Scripts/UI/CSUI/Back.cs
+++ Assets/Scripts/UI/CSUI/Back.cs
@@ -4,5 +4,24 @@ using UnityEngine;
 
 public class Back : MonoBehaviour
 {
-    
+
+    public ConfirmSelect confirmSelect;
+    public HoverPanel hoverPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMouseDown()
+    {
+       
+    }
 }
